c9c562778529a0a77f06d3fb4fc9e941131c0fbe
miche <AhmedSA19980>
2026-09-13 01:08:34 +0300

    feat: add position entity + inter + model

---
+++ backend/Admin.Data/Position.cs
@@ -0,0 +1,45 @@
+﻿using Admin.Core.interfaces.department;
+using Admin.Core.interfaces.Position;
+using Admin.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Data
+{
+    public class PositionRep : IPosition<Position>
+    {
+
+        private readonly AppDbContext _context;
+
+        public PositionRep(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Position> AddAsync(Position enPosition)
+        {
+            await _context.Positions.AddAsync(enPosition);
+            await _context.SaveChangesAsync();
+            return enPosition;
+        }
+
+        public Task<bool> DeactivatePosition(int positionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Position> GetByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePosition(int positionId, string name, string description)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
