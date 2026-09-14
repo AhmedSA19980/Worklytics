58c0fa4082c1952337365641e3b7c8279409547b
miche <AhmedSA19980>
2026-09-15 00:51:48 +0300

    featL update models user + add new fields

--- backend/Admin.Core/models/User.cs
+++ backend/Admin.Core/models/User.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Admin.Core.models
@@ -24,15 +25,31 @@ namespace Admin.Core.models
         [Required, MaxLength(256)]
         public string Email { get; set; }
 
-
-       
         [Required]
+        [ForeignKey("Position")]
+        public int PositionId { get; set; }
+
+        [Required]
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string EmployeeNumber { get; set; }
+
+        [Required]
+        public string HireDate { get; set; }
+
+
+        [ForeignKey("User")]
+        public int? ManagerId { get; set; }
+
+        
         public string Password { get; set; }
 
     
         public DateTime CreatedAt { get; set; }
 
-        public bool IsUserDeleted { get; set; } // 0 =>  false , 1 => true
+        public bool IsActive { get; set; } // 0 =>  false , 1 => true
         public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
         public ICollection<RefreshToken> RefreshToken { get; set; } = new HashSet<RefreshToken>();
 
