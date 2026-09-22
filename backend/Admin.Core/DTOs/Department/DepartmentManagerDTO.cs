60172399623b3bdec0400249934aae0f1d374585
miche <AhmedSA19980>
2026-09-22 19:45:58 +0300

    feat: add 3 classes dtos a-dep b-depManagment  c-updatedep

---
+++ backend/Admin.Core/DTOs/Department/DepartmentManagerDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.DTOs.Department
+{
+    public  class DepartmentManagerDTO
+    {
+
+        public int Id { get; set; }
+
+        public int AssignedByUserId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int RoleId { get; set; }
+
+        public int DepartmentId { get; set; }
+        
+        public string? Report { get; set; }
+    }
+}
