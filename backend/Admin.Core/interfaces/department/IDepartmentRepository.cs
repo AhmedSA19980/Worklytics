68979e97441aea5833036567fdec6a9c1af3cb7f
miche <AhmedSA19980>
2026-09-22 19:51:50 +0300

    interf: remove setManagerDepartment from IDepartment

---
+++ backend/Admin.Core/interfaces/department/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Admin.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.interfaces.department
+{
+    public interface  IDepartmentRepository<T> : IReadRepository<T> , IWriteRepository<T>where T : class
+    {
+        Task<bool> UpdateDepartmentAsync(int departmentId ,string name, string description);
+        Task<bool> DeactivateDepartmentAsync(int departmentId);
+
+       
+        Task<bool> ExistDepartmentByNameAsync(string name, int departmentId);
+    }
+}
